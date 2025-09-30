52da75a8793b35148cc5fb618f055f84394aeb07
TimurGovorovskyi <tgovorovskyi@gmail.com>
2025-09-30 23:13:34 +0300

    Added a menu, a few buttons and customized the design a little.

--- Cookie clicker/game_classes.py
+++ Cookie clicker/game_classes.py
@@ -3,9 +3,10 @@ import pygame
 pygame.init()
 
 text_font = pygame.font.Font(None, 48)
-title = text_font.render("Cookie clicker", True, (0, 0, 0))
+title = text_font.render("Cookie clicker", True, ("#000000"))
+ingame_title = text_font.render("Click the cookie! :D", True, ("#000000"))
 
-click_sound = pygame.mixer.Sound("/home/void/Downloads/Python/Logika/Cookie clicker/assets/click.mp3")
+click_sound = pygame.mixer.Sound("/home/void/Downloads/Python/Logika/Cookie clicker/assets/sounds/click.mp3")
 
 class Game:
     def __init__(self):
@@ -15,7 +16,7 @@ class Game:
         self.clicked = False
         self.color = (255, 223, 0)
 
-        self.upgradeBtn = pygame.Rect(10, 50, 185, 75)
+        self.upgradeBtn = pygame.Rect(300, 60, 185, 75)
         self.upgrade1_cost = 10
 
         self.game_font = pygame.font.Font(None, 20)
@@ -25,15 +26,15 @@ class Game:
         self.display_cost = text_font.render(f"Cost: {self.upgrade1_cost}", True, ("#ffffff"))
 
         pygame.draw.rect(surface, "#883EFF", self.upgradeBtn, border_radius=15)
-        surface.blit(self.display_cost, (35, 80))
-        surface.blit(self.upgrade1_description, (30, 60))
+        surface.blit(self.display_cost, (35 + 285, 80))
+        surface.blit(self.upgrade1_description, (30 + 285, 60))
 
     def draw_score(self, surface):
         self.display_cookies = text_font.render(f"Cookies: {str(self.cookies)}", True, ("#ffffff"))
         surface.blit(self.display_cookies, (25, 550))
 
     def victory(self):
-        if self.cookies >= 100000000:
+        if self.cookies >= 1000000:
             return True
         return False
             
@@ -64,3 +65,42 @@ class Game:
         self.clicked_button(surface)
         self.draw_score(surface)
         self.upgrade(surface)
+
+
+class Button:
+    def __init__(self, x, y, width, height, text, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.color = color
+        self.font = pygame.font.Font(None, 36)
+        self.rect = pygame.Rect(x, y, width, height)
+    
+    def draw(self, surface):
+        pygame.draw.rect(surface, self.color, self.rect, border_radius=15)
+        text_surf = self.font.render(self.text, True, (0, 0, 0))
+        text_rect = text_surf.get_rect(center=self.rect.center)
+        surface.blit(text_surf, text_rect)
+    
+
+    def is_clicked(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if pygame.mouse.get_pressed()[0]:
+                return True
+        return False
+    
+    def change_game_state(self, state):
+        if self.is_clicked():
+            return state
+        return None
+    
+    def is_hovered(self, brighten_color, original_color=None):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            self.color = brighten_color
+        else:
+            if original_color is not None:
+                self.color = original_color
--- Cookie clicker/main.py
+++ Cookie clicker/main.py
@@ -1,5 +1,5 @@
 import pygame
-from game_classes import Game, text_font, title
+from game_classes import Game, text_font, title, Button, ingame_title
 
 pygame.init()
 
@@ -10,26 +10,71 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Cookie clicker")
 cookie_image = pygame.image.load("Cookie clicker/assets/images/Cookie.png").convert_alpha()
 clock = pygame.time.Clock()
+play_button = Button(325, 150, 150, 50, "Play", "#20f79d")
+settings_button = Button(325, 250, 150, 50, "Settings", "#AFAFAF")
+quit_button = Button(325, 350, 150, 50, "Quit", "#ff4c4c")
 
 game = Game()
+running = True
+gamestate = "menu"
 
-while True:
-    screen.fill(("#f5c5ff"))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+if gamestate == "menu":
+    
+    while running:
+        screen.fill(("#f5c5ff"))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+        screen.blit(title, (width // 2 - title.get_width() // 2, 50))
+        
+        play_button.draw(screen)
+        play_button.is_hovered("#9affd5", "#20f79d")
+        if play_button.is_clicked():
+            gamestate = "playing"
+            break
+
+        settings_button.draw(screen)
+        settings_button.is_hovered("#DDDDDD", "#AFAFAF")
+        if settings_button.is_clicked():
+            gamestate = "settings"
+            break
+
+        quit_button.draw(screen)
+        quit_button.is_hovered("#ffacac", "#ff4c4c")
+        if quit_button.is_clicked():
             pygame.quit()
             quit()
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if gamestate == "playing":
         
-    screen.blit(title, (width // 2 - title.get_width() // 2, 50))
+    while running:
 
-    if game.victory():
-        screen.fill(("#49ff95"))  # Fill with black or any color
-        victory_text = text_font.render("You Win! You can close the game now :)", True, (255, 255, 255))
-        screen.blit(victory_text, (screen.get_width() // 2 - victory_text.get_width() // 2,
-                                   screen.get_height() // 2 - victory_text.get_height() // 2))
-    else:
-        game.render(screen)
-        screen.blit(cookie_image, (250, 200))
 
-    pygame.display.flip()
-    clock.tick(60)
+        screen.fill(("#e874ff"))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        if game.victory():
+            screen.fill(("#49ff95"))
+            victory_text = text_font.render("You Win! You can close the game now :)", True, (255, 255, 255))
+            screen.blit(victory_text, (screen.get_width() // 2 - victory_text.get_width() // 2,
+                                    screen.get_height() // 2 - victory_text.get_height() // 2))
+            pygame.mixer_music.play("Victory1.mp3")
+            pygame.mixer_music.play("Victory2.mp3")
+        else:
+            game.render(screen)
+            screen.blit(cookie_image, (250, 200))
+
+        screen.blit(ingame_title, (250, 15))
+
+        
+        pygame.display.flip()
+        clock.tick(60)
